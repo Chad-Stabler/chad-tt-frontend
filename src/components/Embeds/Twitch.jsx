aed4937411d552c9114e3f6999c9036ff34a77fb
Chad Stabler <stablerpsn@gmail.com>
2022-10-13 13:04:29 -0500

    more small troubleshooting on parent string

--- src/components/Embeds/Twitch.jsx
+++ src/components/Embeds/Twitch.jsx
@@ -3,7 +3,7 @@ import styles from './Embeds.css';
 export default function TwitchEmbed({ URL }) {
 
   return <iframe className={styles.TwitchEmbed}
-    src={`${URL}&parent=https://rococo-zuccutto-16e349.netlify.app`}
+    src={`${URL}&parent=rococo-zuccutto-16e349.netlify.app`}
     height="300" width="450"
     allowFullScreen/>;
 }
