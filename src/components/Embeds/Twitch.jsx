92c3d36de03978fdf27fb328622d38c4827ce791
Chad Stabler <stablerpsn@gmail.com>
2022-10-13 13:00:49 -0500

    changing parent param to dev deploy

--- src/components/Embeds/Twitch.jsx
+++ src/components/Embeds/Twitch.jsx
@@ -3,7 +3,7 @@ import styles from './Embeds.css';
 export default function TwitchEmbed({ URL }) {
 
   return <iframe className={styles.TwitchEmbed}
-    src={`${URL}&parent=http://localhost:7891`}
+    src={`${URL}&parent=https://rococo-zuccutto-16e349.netlify.app`}
     height="300" width="450"
     allowFullScreen/>;
 }
