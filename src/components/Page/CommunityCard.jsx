7e7c5a71ff5fff3f4f18b2f45b78e9173eb444c2
sirdan <stanford303@gmail.com>
2022-10-18 14:15:20 -0700

    community css add

--- src/components/Page/CommunityCard.jsx
+++ src/components/Page/CommunityCard.jsx
@@ -1,6 +1,6 @@
 import { getAllUserData } from '../../services/fetch-utils';
 import { useState, useEffect } from 'react';
-import styles from './Profile.css';
+import styles from './CommunityPage.css';
 
 export default function CommunityCard() {
   const [usersData, setUsersData] = useState([]);
@@ -19,8 +19,6 @@ export default function CommunityCard() {
         usersData.map((userData, i) => 
           <div className={styles.CommunityPage}
             key={userData.GamerTag + i}>
-            <h2>{userData.GamerTag}</h2>
-            <img src={userData.avatar_png || '/default.png'}/>
             <p>{userData.platforms}</p>
           </div>
         )}
